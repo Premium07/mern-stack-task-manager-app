e591eb42432310840a355bddad9aad4f7bc90d22
Premium07 <premsagarg23@gmail.com>
2024-09-03 16:24:51 +0545

    cors issue solved

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const app = express();
 
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173", "https://mern-tasker-app.netlify.app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
